aac65424967103ea1ba23ff6b49237085e7cdc4b
Jared Nash <104384562+nashjar000@users.noreply.github.com>
2026-02-26 15:25:29 -0700

    updated some stuff

---
+++ js/config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+    SUPABASE_URL: "https://nyqskysutbfhsivochnx.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55cXNreXN1dGJmaHNpdm9jaG54Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MzQ4NTIsImV4cCI6MjA4NjUxMDg1Mn0.0DQrkbANtxImL0nuHcX-eAtkABnmQHSCKpS-8mNfGvc"
+};
